70e9d76c89b97312ffbc43219c724d7bbbca5221
MerckShrum <79273169+MerckShrum@users.noreply.github.com>
2025-03-07 00:05:14 +0300

    Add ui Frames

---
+++ Lab_1/inc/app.h
@@ -0,0 +1,12 @@
+//
+// Created by merckshrum on 06.03.25.
+//
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <gtk/gtk.h>
+
+void app_activate (GtkApplication *app, gpointer *user_data);
+
+#endif //MAINWINDOW_H
---
+++ Lab_1/src/app.c
@@ -0,0 +1,31 @@
+//
+// Created by merckshrum on 06.03.25.
+//
+
+#include "../inc/app.h"
+
+
+void app_activate (GtkApplication *app, gpointer *user_data) {
+
+    GtkBuilder* builder = gtk_builder_new_from_file("../ui/mainwindow.ui");
+    GObject* window = gtk_builder_get_object (builder, "window");
+    gtk_window_set_application(GTK_WINDOW(window), app);
+
+    // Загрузка CSS
+    GtkCssProvider *css_provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(css_provider, "../ui/styles.css");
+
+    // Применение CSS ко всему приложению
+    GdkDisplay *display = gdk_display_get_default();
+    gtk_style_context_add_provider_for_display(
+        display,
+        GTK_STYLE_PROVIDER(css_provider),
+        GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
+    g_object_unref (builder);
+    // Показ окна
+    gtk_window_present(GTK_WINDOW(window));
+    // gtk_window_set_application (GTK_WINDOW (window), app);
+    // gtk_window_present (GTK_WINDOW (window));
+}
